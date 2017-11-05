ec2dbd7f8803ada32fe1bf87a8b3129a289a23cb
Andrew <andrewrimanic7@gmail.com>
2017-11-05 14:20:58 -0800

    UI test with espresso added. Test covers all possible scenarios for this view

--- app/src/androidTest/java/com/kat/cpen321_ineed/NewPostActivityTest.java
+++ app/src/androidTest/java/com/kat/cpen321_ineed/NewPostActivityTest.java
@@ -18,13 +18,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
@@ -37,125 +36,59 @@ public class NewPostActivityTest {
     public ActivityTestRule<NewPostActivity> mActivityTestRule = new ActivityTestRule<>(NewPostActivity.class);
 
     @Test
-    public void newPostActivityTest() {
+    public void newPostActivityTest2() {
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(60000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
         ViewInteraction editText = onView(
-                allOf(withId(R.id.NameField), withText("Name"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class),
-                                        1),
-                                0),
-                        isDisplayed()));
-        editText.check(matches(withText("Name")));
+                allOf(withId(R.id.NameField)));
+        editText.check(matches(withHint("Name")));
 
         ViewInteraction editText2 = onView(
-                allOf(withId(R.id.DescriptionField), withText("Description"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class),
-                                        1),
-                                1),
-                        isDisplayed()));
-        editText2.check(matches(withText("Description")));
+                allOf(withId(R.id.DescriptionField)));
+        editText2.check(matches(withHint("Description")));
 
         ViewInteraction editText3 = onView(
-                allOf(withId(R.id.PriceField), withText("$"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class),
-                                        1),
-                                2),
-                        isDisplayed()));
-        editText3.check(matches(withText("$")));
-
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.NameField), isDisplayed()));
-        appCompatEditText.perform(replaceText(""), closeSoftKeyboard());
-
-        pressBack();
+                allOf(withId(R.id.PriceField)));
+        editText3.check(matches(withHint("$")));
 
         ViewInteraction appCompatButton = onView(
                 allOf(withId(R.id.CreatePostButton), withText("Confirm"), isDisplayed()));
         appCompatButton.perform(click());
 
         ViewInteraction editText4 = onView(
-                allOf(withId(R.id.NameField), withText("Must enter a name"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class),
-                                        1),
-                                0),
-                        isDisplayed()));
-        editText4.check(matches(withText("Must enter a name")));
+                allOf(withId(R.id.NameField)));
+        editText4.check(matches(withHint("Must enter a name")));
 
         ViewInteraction editText5 = onView(
-                allOf(withId(R.id.DescriptionField), withText("Must enter a description"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class),
-                                        1),
-                                1),
-                        isDisplayed()));
-        editText5.check(matches(withText("Must enter a description")));
+                allOf(withId(R.id.DescriptionField)));
+        editText5.check(matches(withHint("Must enter a description")));
 
         ViewInteraction editText6 = onView(
-                allOf(withId(R.id.PriceField), withText("Must enter a price"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class),
-                                        1),
-                                2),
-                        isDisplayed()));
-        editText6.check(matches(withText("Must enter a price")));
+                allOf(withId(R.id.PriceField)));
+        editText6.check(matches(withHint("Must enter a price")));
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.NameField), isDisplayed()));
+        appCompatEditText.perform(replaceText("Espresso"), closeSoftKeyboard());
 
         ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.NameField), isDisplayed()));
-        appCompatEditText2.perform(click());
+                allOf(withId(R.id.DescriptionField), isDisplayed()));
+        appCompatEditText2.perform(replaceText("test\n"), closeSoftKeyboard());
 
         ViewInteraction appCompatEditText3 = onView(
-                allOf(withId(R.id.NameField), isDisplayed()));
-        appCompatEditText3.perform(click());
-
-        ViewInteraction appCompatEditText4 = onView(
-                allOf(withId(R.id.NameField), isDisplayed()));
-        appCompatEditText4.perform(replaceText("Espresso"), closeSoftKeyboard());
-
-        pressBack();
+                allOf(withId(R.id.PriceField), isDisplayed()));
+        appCompatEditText3.perform(replaceText("56"), closeSoftKeyboard());
 
         ViewInteraction appCompatButton2 = onView(
                 allOf(withId(R.id.CreatePostButton), withText("Confirm"), isDisplayed()));
         appCompatButton2.perform(click());
-
-        ViewInteraction appCompatEditText5 = onView(
-                allOf(withId(R.id.DescriptionField), isDisplayed()));
-        appCompatEditText5.perform(replaceText("Test"), closeSoftKeyboard());
-
-        pressBack();
-
-        ViewInteraction appCompatButton3 = onView(
-                allOf(withId(R.id.CreatePostButton), withText("Confirm"), isDisplayed()));
-        appCompatButton3.perform(click());
-
-        ViewInteraction appCompatEditText6 = onView(
-                allOf(withId(R.id.PriceField), isDisplayed()));
-        appCompatEditText6.perform(replaceText("5"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText7 = onView(
-                allOf(withId(R.id.PriceField), withText("5"), isDisplayed()));
-        appCompatEditText7.perform(pressImeActionButton());
-
-        ViewInteraction appCompatButton4 = onView(
-                allOf(withId(R.id.CreatePostButton), withText("Confirm"), isDisplayed()));
-        appCompatButton4.perform(click());
 
     }
 
